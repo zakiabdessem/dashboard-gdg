b1ee64df62a8a1b09d583ad3d3cd05dbbff62639
zak <zakiabdessemed35@gmail.com>
2024-12-05 13:12:20 +0100

    Added things

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,4 +1,3 @@
-export const BACKEND_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "https://api.gdg-batna.com";
+export const BACKEND_URL = "https://api.gdg-batna.com";
 
 export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL + "/api";
